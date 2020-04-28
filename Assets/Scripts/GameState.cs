8b45c37cc1f950abb2a3f8dcef8a3e2c02d71ad6
Tiago Cabral <riskybomberpt@gmail.com>
2020-04-28 19:18:20 +0100

    [Add] Text Writing Effect
    Added Text Writing Effect and coded the Text
    Status Indicators into the Writing functions.

--- Assets/Scripts/GameState.cs
+++ Assets/Scripts/GameState.cs
@@ -13,6 +13,8 @@ public class GameState : MonoBehaviour {
     public GameObject TwoOptionsBox;
     public GameObject ThreeOptionsBox;
     public GameObject FourOptionsBox;
+    public GameObject DialogueLoadingIndicator;
+    public GameObject DialogueSkipIndicator;
 
     private int CurrentChapterNumber = 1;
     private int CurrentDialogueNumber = 1;
@@ -36,11 +38,14 @@ public class GameState : MonoBehaviour {
 
     // Update is called once per frame
     void Update() {
-
+        WriteUpdate();
     }
 
-    void loadDialogue(int ChapterNumber, int DialogueNumber) {
+    private void loadDialogue(int ChapterNumber, int DialogueNumber) {
         Debug.Log("loadDialogue called with " + ChapterNumber + " and " + DialogueNumber);
+
+        // Making sure the OptionsBoxes are all disabled at the beginning of a new dialogue
+        setActiveOptionsBoxes();
 
         // Calling -1 for chapter and dialogue moves to EndScene
         if (ChapterNumber == -1 && DialogueNumber == -1) {
@@ -65,33 +70,35 @@ public class GameState : MonoBehaviour {
         CurrentChapter = persistent.ChapterList[ChapterNumber];
         CurrentDialogue = CurrentChapter.Dialogues[DialogueNumber];
 
-        dialogueBoxText.text = CurrentDialogue.Text;
         characterNameBoxText.text = CurrentDialogue.Character;
-
-        if (CurrentDialogue.IsQuestion()) {
-            int NumberOfOptions = CurrentDialogue.Question.Options.Count;
-            setActiveOptionsBoxes(NumberOfOptions);
-            for (int x = 0; x < CurrentDialogue.Question.Options.Count; x++) {
-                GameObject OptionButton = OptionsBoxes[NumberOfOptions].transform.GetChild(x).gameObject;
-                Text TextField = OptionButton.transform.GetChild(0).GetComponent<Text>();
-                TextField.text = CurrentDialogue.Question.Options[x].Text;
-            }
-        }
-        else {
-            setActiveOptionsBoxes();
-        }
+        WriteText(CurrentDialogue.Text);
 
     }
 
     public void clickDialogue() {
-        if (!CurrentDialogue.IsQuestion()) {
-            if (CurrentDialogue.HasRedirect()) {
-                loadDialogue(CurrentDialogue.Redirect.Chapter, CurrentDialogue.Redirect.Dialogue);
-            }
-            else {
-                loadDialogue(CurrentChapterNumber, ++CurrentDialogueNumber);
+        if (!Writing) {
+            if (!CurrentDialogue.IsQuestion()) {
+                if (CurrentDialogue.HasRedirect()) {
+                    loadDialogue(CurrentDialogue.Redirect.Chapter, CurrentDialogue.Redirect.Dialogue);
+                }
+                else {
+                    loadDialogue(CurrentChapterNumber, ++CurrentDialogueNumber);
+                }
             }
         }
+        else {
+            WriteAll();
+        }
+    }
+
+    public void SetOptions() {
+        int NumberOfOptions = CurrentDialogue.Question.Options.Count;
+        for (int x = 0; x < CurrentDialogue.Question.Options.Count; x++) {
+            GameObject OptionButton = OptionsBoxes[NumberOfOptions].transform.GetChild(x).gameObject;
+            Text TextField = OptionButton.transform.GetChild(0).GetComponent<Text>();
+            TextField.text = CurrentDialogue.Question.Options[x].Text;
+        }
+        setActiveOptionsBoxes(NumberOfOptions);
     }
 
     // Each button should have an option number assigned, starting from 0
@@ -110,6 +117,76 @@ public class GameState : MonoBehaviour {
         }
         else {
             OptionsBoxes[NumberOfOptions].SetActive(true);
+        }
+    }
+
+    // Text writing stuff
+    private bool Writing = false;
+    private string TextToWrite = "";
+    private int TextToWriteIndex = 0;
+    private float WriteTimer = 0f;
+    public float TimePerCharacter = .1f;
+
+
+    private void WriteText(string textToWrite) {
+        WriteTimer = 0f;
+        TextToWrite = textToWrite;
+        TextToWriteIndex = 0;
+        Writing = true;
+    }
+
+    private void WriteAll() {
+        Writing = false;
+        dialogueBoxText.text = TextToWrite;
+    }
+
+    private void WriteUpdate() {
+        if (Writing) {
+            SetWriteIndicator(1);
+            WriteTimer += Time.deltaTime;
+            if (WriteTimer > TimePerCharacter) {
+                int charactersToWrite = (int)(WriteTimer / TimePerCharacter);
+                WriteTimer -= (charactersToWrite*TimePerCharacter);
+                TextToWriteIndex += charactersToWrite;
+
+                if (TextToWriteIndex >= TextToWrite.Length) {
+                    Writing = false;
+                    dialogueBoxText.text = TextToWrite;
+                    WriteUpdate(); // Call itself to call SetWriteIndicator appropriately
+                }
+                else {
+                    string formattedText = TextToWrite.Substring(0, TextToWriteIndex);
+                    formattedText += "<color=#00000000>" + TextToWrite.Substring(TextToWriteIndex) + "</color>";
+                    dialogueBoxText.text = formattedText;
+                }
+            }
+        }
+        else {
+            if (!CurrentDialogue.IsQuestion()) {
+                SetWriteIndicator(0);
+            }
+            else {
+                SetWriteIndicator();
+                SetOptions();
+            }
+        }
+    }
+
+    private void SetWriteIndicator(int status = -1) {
+        // Anything: All off; 0: Skip indicator; 1: Writing indicator
+        switch (status) {
+            case 0:
+                DialogueSkipIndicator.SetActive(true);
+                DialogueLoadingIndicator.SetActive(false);
+                break;
+            case 1:
+                DialogueSkipIndicator.SetActive(false);
+                DialogueLoadingIndicator.SetActive(true);
+                break;
+            default:
+                DialogueSkipIndicator.SetActive(false);
+                DialogueLoadingIndicator.SetActive(false);
+                break;
         }
     }
 }
