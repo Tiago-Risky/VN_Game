b1c4d840ada8b3e2cf18b6869702aa69f765671a
Tiago Cabral <riskybomberpt@gmail.com>
2020-05-16 19:12:01 +0100

    [Add] Character class
    Adding Character class

---
+++ Assets/Scripts/VisualNovel/Character.cs
@@ -0,0 +1,19 @@
+﻿namespace VisualNovel {
+    public class Character {
+        public string Name = "";
+        public string Picture = "";
+        public int Side; // 0 = Left; 1 = Center; 2 = Right
+        public bool Selected;
+
+        public Character(string name, string picture, int side, bool selected = false) {
+            Name = name;
+            Picture = picture;
+            Side = side;
+            Selected = selected;
+        }
+
+        public bool HasPicture() {
+            return Picture.Length > 0;
+        }
+    }
+}
